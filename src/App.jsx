c4eff3a4bddc84ec033b6c84485bdf83363c7543
AlMoataz-M <moatzmohammed222@yahoo.com>
2024-09-25 02:36:33 +0300

    first navbar and home

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import "./App.css";
-
+import Home from "./pages/home/home";
+import Layout from "./pages/layout/layout";
 function App() {
-  return <></>;
+  return (
+    <>
+      <Layout />
+      <Home />
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/home/home.jsx
@@ -0,0 +1,8 @@
+import styling from "./home.module.css";
+export default function Home() {
+  return (
+    <>
+      <div className={styling.home}></div>
+    </>
+  );
+}
